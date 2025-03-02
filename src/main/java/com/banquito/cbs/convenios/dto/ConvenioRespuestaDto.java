bc72af9c3c0f92a778579e32691284e9d91a2ab0
Deivy Steven Solorzano Briones <dssolorzano1@espe.edu.ec>
2025-03-02 14:44:28 -0500

    cambio

--- src/main/java/com/banquito/cbs/convenios/dto/ConvenioRespuestaDto.java
+++ src/main/java/com/banquito/cbs/convenios/dto/ConvenioRespuestaDto.java
@@ -1,7 +1,16 @@
 package com.banquito.cbs.convenios.dto;
 
 import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
-public class ConvenioRespuestaDto {
-} 
+public class ConvenioRespuestaDTO {
+    private Integer id;
+    private Integer idCuenta;
+    private Integer mesesDiferidos;
+    private BigDecimal montoMinimo;
+    private String tipoDiferido;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+}
--- src/main/java/com/banquito/cbs/convenios/dto/ErrorDto.java
+++ src/main/java/com/banquito/cbs/convenios/dto/ErrorDto.java
@@ -1,10 +1,8 @@
 package com.banquito.cbs.convenios.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
-public class ErrorDto {
-
-} 
+public class ErrorDTO {
+    private String mensaje;
+}
