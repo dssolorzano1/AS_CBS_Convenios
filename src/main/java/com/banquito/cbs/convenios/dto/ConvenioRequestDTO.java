96f4e5e6ce0b554c0b7a784d191b5c08a025c630
Deivy Steven Solorzano Briones <78752820+dssolorzano1@users.noreply.github.com>
2025-03-04 21:47:09 -0500

    Original

--- src/main/java/com/banquito/cbs/convenios/dto/ConvenioRequestDTO.java
+++ src/main/java/com/banquito/cbs/convenios/dto/ConvenioRequestDTO.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class ConvenioRequestDTO {
+    private Integer id;
     private Integer idCuenta;
     private Integer mesesDiferidos;
     private BigDecimal montoMinimo;
